fddcb466fca3a7c7528cb077d1160cfc3d2f9650
shezan14 <shezan789@yahoo.com>
2016-02-13 00:19:16 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Restaurant Reservation</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+	<script src="js/jquery-ui.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/sticky-footer.css">
+	<link href="css/bootstrap-lightbox.css" rel="stylesheet" />
+	<script src="js/bootstrap-lightbox.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+							</div>
+			<div>
+				<ul class="nav navbar-nav">
+					<!-- <li class="active"><a href="#">Home</a></li> -->
+					<li><a href="index.php">Home</a></li>
+					<li><a href="menu.php">Menu</a></li>
+					<li><a href="reservation.php">Reservation</a></li>
+					<li><a href="location.php">Locations</a></li>
+					<li><a href="about.php">About</a></li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+					<li><a href="adminlogin.php">Admin Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>	
+	<div class="container-fluid">
+		<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <title>Amazing Slider</title>
+    
+    <!-- Insert to your webpage before the </head> -->
+    <script src="sliderengine/jquery.js"></script>
+    <script src="sliderengine/amazingslider.js"></script>
+    <link rel="stylesheet" type="text/css" href="sliderengine/amazingslider-1.css">
+    <script src="sliderengine/initslider-1.js"></script>
+    <!-- End of head section HTML codes -->
+    
+    <style type="text/css">
+        body, html {
+        margin: 0;
+        padding: 0;
+        }
+    </style>
+
+</head>
+<body>
+    
+    <!-- Insert to your webpage where you want to display the slider -->
+    <div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:100%;margin:0 auto;">
+        <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
+            <ul class="amazingslider-slides" style="display:none;">
+                <li><img src="simages/10173806_403955693102178_1967009652446298067_n.jpg" alt="10173806_403955693102178_1967009652446298067_n" data-duration="3500" />
+                </li>
+                <li><img src="simages/10690322_837977022927165_2540045037897158844_n.jpg" alt="10690322_837977022927165_2540045037897158844_n" data-duration="2500" />
+                </li>
+                <li><img src="simages/10169362_1476546135909361_2147247850225949237_n.jpg" alt="10169362_1476546135909361_2147247850225949237_n" />
+                </li>
+                <li><img src="simages/10352296_355161528011316_1828028055376574128_n.jpg" alt="10352296_355161528011316_1828028055376574128_n" />
+                </li>
+            </ul>
+            <ul class="amazingslider-thumbnails" style="display:none;">
+                <li><img src="simages/10173806_403955693102178_1967009652446298067_n-tn.jpg" alt="10173806_403955693102178_1967009652446298067_n" /></li>
+                <li><img src="simages/10690322_837977022927165_2540045037897158844_n-tn.jpg" alt="10690322_837977022927165_2540045037897158844_n" /></li>
+                <li><img src="simages/10169362_1476546135909361_2147247850225949237_n-tn.jpg" alt="10169362_1476546135909361_2147247850225949237_n" /></li>
+                <li><img src="simages/10352296_355161528011316_1828028055376574128_n-tn.jpg" alt="10352296_355161528011316_1828028055376574128_n" /></li>
+            </ul>
+        </div>
+    </div>
+    <!-- End of body section HTML codes -->
+    
+</body>
+</html>		<div>
+			<h1>&nbsp;</h1>
+			<div class="jumbotron" >
+				<div class="row" >
+					<div class="col-sm-6">
+						We are a team of dedicated, food enthusiasts who welcomes you to order food online and have it delivered to your doorsteps. We truly aim to serve other food enthusiasts long term and in international standards. You no longer have to wait in queues for takeaways or sit in traffic jam while the food gets cold or even carry hot packed food. 
+					</div>
+					<div class="col-sm-6">
+						We human beings are always hungry , 
+					</div>
+				</div>
+			</div>
+			<div class="row" style="padding-top: 10px;">
+				<div class="col-sm-4"><img src="images/img/f1.jpg"></div>
+				<div class="col-sm-4"><img src="images/img/f2.jpg"></div>
+				<div class="col-sm-4"><img src="images/img/f3.jpg"></div>
+				<div class="col-sm-4"><img src="images/img/f4.jpg"></div>
+				<div class="col-sm-4"><img src="images/img/f5.jpg"></div>
+				<div class="col-sm-4"><img src="images/img/f6.jpg"></div>
+			</div>
+		</div>           
+		<div id="footer">
+	<div class="container">
+		<hr>
+		<p class="muted credit">Web Programming Project By Shezan & Afrida.</p>
+		<hr>
+	</div>
+</div>
+
+<!-- <div id="footer">
+	<div class="container">
+		<p class="muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+	</div>
+</div> -->	</div>
+
+
+</body>
+</html>
